5e8a3973be0facad2bdfbee335d4b63f29c49947
Ngan <contact.nghiemngan@gmail.com>
2026-03-24 14:32:01 +0100

    fix encoding

--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
 import {clear} from "node:console";
 import fs from "node:fs"
 
-let data = fs.readFileSync("./data.txt", "uft-8");
+let data = fs.readFileSync("./data.txt", "utf-8");
 
 console.log(data);
